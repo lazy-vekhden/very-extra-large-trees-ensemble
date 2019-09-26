8c90635a5e67da3863f0ff39f4e101383d8c27a5
wetleather <wetleather@yandex-team.ru>
2019-09-26 11:56:50 +0300

    Initial - accumulation code

---
+++ src/com/labean/velet/data/ThreadSafeAccumulator.java
@@ -0,0 +1,46 @@
+package com.labean.velet.data;
+
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.IntStream;
+
+public class ThreadSafeAccumulator {
+    private static final int PFS = 100;
+
+    private final int testSize;
+    private final int rowsCount;
+    private final double[][] accumulator;
+
+    private final AtomicInteger initialMaker;
+    private final ThreadLocal<Integer> rowForThread;
+
+    public ThreadSafeAccumulator(int testSize) {
+        this.testSize = testSize;
+        this.rowsCount = Runtime.getRuntime().availableProcessors();
+        this.accumulator = new double[rowsCount][testSize + PFS];
+
+        this.initialMaker = new AtomicInteger(0);
+        rowForThread = ThreadLocal.withInitial(() -> initialMaker.getAndIncrement());
+    }
+
+    public void addToList(List<Integer> ls, double value) {
+        if (value == 0) return;
+        if (ls.size() == 0) return;
+        int row = rowForThread.get();
+        for (int i : ls) {
+            if (i >= 0 && i < testSize) {
+                accumulator[row][i] += value;
+            }
+        }
+    }
+
+    public double[] getResult() {
+        return IntStream.range(0, testSize)
+                .parallel()
+                .mapToDouble(
+                        i -> IntStream.range(0, rowsCount)
+                                .mapToDouble(j -> accumulator[j][i])
+                                .sum())
+                .toArray();
+    }
+}
